2d96a413245a2f9b1663f13a2f3e6c494b1717fb
jfiliceatcsumb <jfilice@csumb.edu>
2024-04-22 15:29:46 -0700

    Update post-macos-upgrade-recon.jss.sh

--- post-macos-upgrade-recon.jss.sh
+++ post-macos-upgrade-recon.jss.sh
@@ -1,7 +1,59 @@
-#!/bin/bash
+#!/bin/zsh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+# This script requires Jamf Pro binary to be installed.
+# Run it with no arguments. 
+# 
+# Use as script in Jamf JSS.
+
+# https://github.com/jfiliceatcsumb/recon-after-macos-update
+
+# Change History:
+# 2024/04/22:	forked from alectrona/recon-after-macos-update https://github.com/alectrona/recon-after-macos-update
+# 2024/04/22:	Modified to run as Jamf Pro policy scripts.
+# 2024/04/22:	Changed functionality to read the OS build number rather than the OS version number. 
+# 				Build number will always change when the OS is updated, even when the OS version number does not always change after an update.
+# 				Credit: https://docs.google.com/document/d/1QrfX9WBzG2Z3fCt5n9grpu6hpyvRkSQ6RUNHOCNwCXs/edit#bookmark=id.k2szjaauy98q
+# 
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
+
+# ##########################################
+# VARIABLES - edit to suite your organization and preferred locations
+# ##########################################
+# 
+
+
+set -x
 
 currentOSVersion=$(/usr/bin/sw_vers -productVersion)
-lastRecordedOSVersion=$(/usr/bin/defaults read /Library/Preferences/com.alectrona.configuration.plist OS_Version 2> /dev/null)
+currentOSBuild=$(/usr/bin/sw_vers --buildVersion)
+lastRecordedOSVersion=$(/usr/bin/defaults read /Library/Preferences/edu.csumb.custom.plist OS_Version 2> /dev/null)
+lastRecordedOSBuild=$(/usr/bin/defaults read /Library/Preferences/edu.csumb.custom.plist OS_Build 2> /dev/null)
+# ##########################################
+
 
 function initial_network_test () {
     # Include rc.common for the CheckForNetwork function
@@ -56,12 +108,14 @@ initial_network_test
 external_dns_lookup_test
 
 # If we have recorded the OS version before, check to see if our recorded value matches the current version
-if [[ -n "$lastRecordedOSVersion" ]]; then
-    if [[ ! "$currentOSVersion" == "$lastRecordedOSVersion" ]]; then
-        echo "This Mac has been updated from $lastRecordedOSVersion to $currentOSVersion; running recon."
-        if /usr/local/bin/jamf recon; then
+if [[ -n "$lastRecordedOSBuild" ]]; then
+    if [[ ! "$currentOSBuild" == "$lastRecordedOSBuild" ]]; then
+        echo "This Mac has been updated from $lastRecordedOSVersion ($lastRecordedOSBuild) to $currentOSVersion ($currentOSBuild); running jamf recon..."
+        if /usr/local/bin/jamf recon # jamf recon successful
+        then
             # Record the current OS version to use as comparison upon next run
-            /usr/bin/defaults write /Library/Preferences/com.alectrona.configuration.plist OS_Version "$currentOSVersion"
+            /usr/bin/defaults write /Library/Preferences/edu.csumb.custom.plist OS_Version "$currentOSVersion"
+            /usr/bin/defaults write /Library/Preferences/edu.csumb.custom.plist OS_Build "$currentOSBuild"
         fi
     else
         echo "No change in macOS version detected."
@@ -69,7 +123,8 @@ if [[ -n "$lastRecordedOSVersion" ]]; then
 else
     echo "This appears to be the first run; initializing plist."
     # Record the current OS version to use as comparison upon next run
-    /usr/bin/defaults write /Library/Preferences/com.alectrona.configuration.plist OS_Version "$currentOSVersion"
+    /usr/bin/defaults write /Library/Preferences/edu.csumb.custom.plist OS_Version "$currentOSVersion"
+    /usr/bin/defaults write /Library/Preferences/edu.csumb.custom.plist OS_Build "$currentOSBuild"
 fi
 
-exit 0
+exit
